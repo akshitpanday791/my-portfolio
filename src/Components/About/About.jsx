5b91b5b4422bcd0cd2a6d9a61c3c636c028407b7
Akshit Panday <akshitpanday791@outlook.com>
2021-05-06 18:59:53 +0530

    minor changes

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Window from './Window/Window';
 import {Grid} from '@material-ui/core';
+import data from '../../data/about.json';
 
 import useStyles from './styles';
 
@@ -17,11 +18,8 @@ const About = () => {
 
                 <Grid item xs={12} sm={8} className={classes.window}>
                     <Window>
-                        <h3 className={classes.intro}>Hi :)</h3>
-                        <p className={classes.info}>👋I'm Akshit Panday from Nagpur, India. I am a Web Developer and Programmer currently pursing a Bachelors Degree in 
-                        Computer Science. After my graduation I'm interested in working in some reputed IT-company for my
-                        professional as well as personal growth. I'm a Tech enthusiast who loves to build full stack Web apps & Mobile apps, Open source contributions and to explore latest frameworks and technologies in field of Computer Science.  Apart from this I like to play online video games
-                            and travelling.</p>
+                        <h3 className={classes.intro}>{data.headline}</h3>
+                        <p className={classes.info}>{data.info}</p>
                     </Window>
                 </Grid>
 
